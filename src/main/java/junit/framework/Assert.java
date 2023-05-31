c1279bda53efba902e8352c61f7d42bec2334fc7
Rachel-oc <mengting.xie@thoughtworks.com>
2023-05-31 16:28:12 +0800

    initial code and tests

---
+++ src/main/java/junit/framework/Assert.java
@@ -0,0 +1,22 @@
+package junit.framework;
+
+public class Assert {
+
+    private Assert() {
+
+    }
+
+    public static String format(String message, String fExpected, String fActual) {
+        StringBuilder sb = new StringBuilder();
+        if (message != null) {
+            sb.append(message)
+                    .append(" ");
+        }
+        sb.append("expected:<")
+                .append(fExpected)
+                .append("> but was:<")
+                .append(fActual)
+                .append(">");
+        return sb.toString();
+    }
+}
